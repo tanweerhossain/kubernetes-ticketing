a4993bd015d32d25193f97a0714fe894810c6feb
tanweerhossain <tanweerhossainatspacebbsr@gmail.com>
2021-09-20 19:41:53 +0530

    Topic: Update Common Microservice
    Status: Done

--- common/src/interface/expiration-complete-event.ts
+++ common/src/interface/expiration-complete-event.ts
@@ -1,6 +1,7 @@
 import { Subjects } from "../enums/subjects";
+import { Event } from "./event";
 
-export interface ExpirationComplete {
+export interface ExpirationCompleteEvent extends Event {
   subject: Subjects.ExpirationComplete;
   data: {
     orderId: string;
